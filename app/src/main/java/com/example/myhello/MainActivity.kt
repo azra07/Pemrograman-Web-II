0e86edf15b92d6c2a078c5455643d807cc9fa10c
azra07 <fatimaazhara141005@gmail.com>
2026-02-12 14:10:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myhello/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.myhello
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.app.AppCompatDelegate
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val etNama = findViewById<EditText>(R.id.etNama)
+        val btnSapa = findViewById<Button>(R.id.btnSapa)
+        val tvHasil = findViewById<TextView>(R.id.tvHasil)
+        val switchMode = findViewById<Switch>(R.id.switchMode)
+
+        btnSapa.setOnClickListener {
+            val nama = etNama.text.toString()
+            tvHasil.text = "Hello $nama!"
+        }
+
+        switchMode.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+    }
+}
